7fe18be9fbf75fcaf2b414931a5280b7a3033e7f
Assan Hedqvist <secka@kth.se>
2024-10-09 19:24:06 +0200

    Followed Anders tutorial videos to 4B, details for projects still need to be implemented.

---
+++ ProjectApp/Controllers/ProjectsController.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Evaluation;
+using ProjectApp.Core.Interfaces;
+using ProjectApp.Models.Projects;
+using Project = ProjectApp.Core.Project;
+
+namespace ProjectApp.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        
+        private IProjectService _projectService;
+        
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+        // GET: ProjectsController
+        public ActionResult Index()
+        {
+            List<Project> projects = _projectService.GetAllByUserName("dummy");
+            List<ProjectVm> projectsVms = new List<ProjectVm>();
+            foreach (var project in projects)
+            {
+                projectsVms.Add(ProjectVm.FromProject(project));
+            }
+            return View(projectsVms);
+        }
+
+        // GET: ProjectsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    
+        // GET: ProjectsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProjectApp/Core/ProjectService.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using ProjectApp.Core.Interfaces;
+
+
+namespace ProjectApp.Core;
+
+public class ProjectService : IProjectService
+{
+    private readonly IProjectPersistence _projectPersistence;
+
+    public ProjectService(IProjectPersistence projectPersistence)
+    {
+        _projectPersistence = projectPersistence;
+    }
+    
+    public List<Project> GetAllByUserName(string userName)
+    {
+        List<Project> projects = _projectPersistence.GetAllByUserName(userName);
+        return projects;
+    }
+
+    public Project GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Project GetById(int id, string userName)
+    {
+        Project project = _projectPersistence.GetById(id, userName);
+        if(project == null) throw new DataException("project not found");
+        return project;
+    }
+
+    public void Add(string userName, string title)
+    {
+        throw new NotImplementedException("Add");
+    }
+}
